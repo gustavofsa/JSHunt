c3835235ea528318c5ede53b2057dd741b776c2c
Gustavo Farias <gugafarias2009@gmail.com>
2020-05-28 16:41:54 -0300

    add header link + product details page and style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './style.css';
 
 const Header = () => (
-  <header id="main-header">JSHunt</header>
+  <header id="main-header">
+    <a href="/">JSHunt</a>
+  </header>
 )
 
 export default Header;
